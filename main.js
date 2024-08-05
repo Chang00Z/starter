51ba12f734eb3200e11398dd04ac4b0960c36285
oahehc <oahehc@gmail.com>
2024-08-05 22:20:41 +0900

    feat: sort

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ document.querySelector(".container").addEventListener("click", (event) => {
     event.target.classList.toggle("delete");
   }
 });
+
+let isReverse = false;
+document.querySelector(".sortButton").addEventListener("click", () => {
+  const container = document.querySelector(".container");
+  const itemsArray = Array.from(container.querySelectorAll("div"));
+  itemsArray.sort((a, b) => {
+    const textA = a.textContent.trim();
+    const textB = b.textContent.trim();
+    if (isReverse) {
+      return textB.localeCompare(textA, "zh");
+    }
+    return textA.localeCompare(textB, "zh");
+  });
+  isReverse = !isReverse;
+
+  container.innerHTML = "";
+  itemsArray.forEach((item) => container.appendChild(item));
+});
